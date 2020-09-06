e23434332576383442ac89100fb8cc1d59bc807a
abpmad <donghe1216@gmail.com>
2020-09-06 01:13:31 -0700

    Add Exception Handlering mechanism

--- src/main/java/com/adbpmad/ppmtool/services/ProjectService.java
+++ src/main/java/com/adbpmad/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.adbpmad.ppmtool.services;
 
 import com.adbpmad.ppmtool.domain.Project;
+import com.adbpmad.ppmtool.exceptions.ProjectIdException;
 import com.adbpmad.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+
+        }catch (Exception e){
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase()+"' already exists.");
+
+        }
     }
 }
