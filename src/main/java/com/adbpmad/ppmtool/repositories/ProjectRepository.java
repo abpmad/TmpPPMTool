1877f65bc2ea53cec6c7d5aff9708f618d6706c3
abpmad <donghe1216@gmail.com>
2020-09-05 22:25:14 -0700

    Project Obejct Validataion part2

---
+++ src/main/java/com/adbpmad/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.adbpmad.ppmtool.repositories;
+
+import com.adbpmad.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
---
+++ src/main/java/com/adbpmad/ppmtool/services/ProjectService.java
@@ -0,0 +1,16 @@
+package com.adbpmad.ppmtool.services;
+
+import com.adbpmad.ppmtool.domain.Project;
+import com.adbpmad.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+}
